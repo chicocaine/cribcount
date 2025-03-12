e6b52f6f1a84d3f28f8f3915d167d69968c1ca4c
Robroi Neo M. Dingal <robroineo2@gmail.com>
2025-03-12 08:46:39 +0800

    changed border color for save and load

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="inline-flex items-center rounded-md border border-transparent bg-zinc-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-zinc-700 focus:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 active:bg-zinc-900 dark:bg-zinc-200 dark:text-zinc-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-zinc-800 dark:active:bg-zinc-300"
+        class="inline-flex items-center rounded-md border border-transparent bg-zinc-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-zinc-700 focus:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 active:bg-zinc-900 dark:bg-zinc-200 dark:text-zinc-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-zinc-800 dark:active:bg-zinc-300"
     >
         <slot />
     </button>
